a76f80ad85be756be79ccf48474942ac5c1f5003
saleh rashid <saleh.rashid.b@gmailcom>
2022-01-27 13:23:25 +0700

    initial commit

--- app/src/main/java/com/app/smartalarm/MainActivity.kt
+++ app/src/main/java/com/app/smartalarm/MainActivity.kt
@@ -3,21 +3,38 @@ package com.app.smartalarm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.smartalarm.adapter.AlarmAdapter
+import com.app.smartalarm.data.local.AlarmDB
 import com.app.smartalarm.databinding.ActivityMainBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding// adalah class yang menampung layout
+    private var _binding: ActivityMainBinding? = null// adalah class yang menampung layout
+    private val binding get() = _binding as ActivityMainBinding
+
+    private var alarmAdapter: AlarmAdapter? = null
+    private val db by lazy { AlarmDB(this) }//lazy adalah mempermudah dalam inisialisasi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initView()
+        setupRecylerView()
+    }
+
+    private fun setupRecylerView() {
+        binding.apply {
+            rvReminderAlarm.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = alarmAdapter
+            }
+        }
     }
 
     private fun initView(){
--- app/src/main/java/com/app/smartalarm/data/local/AlarmDao.kt
+++ app/src/main/java/com/app/smartalarm/data/local/AlarmDao.kt
@@ -2,10 +2,14 @@ package com.app.smartalarm.data.local
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.app.smartalarm.data.Alarm
 
 @Dao
 interface AlarmDao {
     @Insert
     fun addAlarm(alarm: Alarm)
+
+    @Query("SELECT * FROM alarm")
+    fun getAlarm() : List<Alarm>
 }
